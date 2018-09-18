7e08c76a4f8234ac40841c6110e62f2d64eaefc6
2n8asm <2n8asm@gmail.com>
2018-09-18 10:55:54 +0800

    上传了教学周 01 的作业

--- 02/quadratic_equation.cpp
+++ 02/quadratic_equation.cpp
@@ -4,14 +4,13 @@
 int main()
 {
     long double eps = 1e-6;
-    long double neg_eps = -eps;
     long double a, b, c, delta;
 
     std::cin >> a >> b >> c;
 
     delta = b * b - 4 * a * c;
 
-    if(delta < neg_eps)
+    if (-delta >= eps)
         std::cout << "no root";
     else
     {
@@ -21,15 +20,15 @@ int main()
         long double x2 = (b - sqrt_d) / neg_2a;
         long double temp;
 
-        if(x1 - x2 > eps)
+        if (x1 - x2 >= eps)
             temp = x1, x1 = x2, x2 = temp;
-        if(neg_eps < x1 && x1 < eps)
+        if (std::abs(x1) < eps)
             x1 = 0;
-        if(neg_eps < x2 && x2 < eps)
+        if (std::abs(x2) < eps)
             x2 = 0;
 
         std::cout << x1;
-        if(x2 - x1 > eps)
+        if (x2 - x1 >= eps)
             std::cout << ' ' << x2;
     }
 
