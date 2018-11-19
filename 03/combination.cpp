91cf6eed09d6d3fae2d0a9f4ce49394798c89d19
2n8asm <2n8asm@gmail.com>
2018-11-20 00:55:42 +0800

    上传了教学周 03 的作业

--- 03/combination.cpp
+++ 03/combination.cpp
@@ -1,19 +1,15 @@
 #include <iostream>
 
-void C(int n, int r, unsigned long long c = 0)
+void C(int n, int r, int c = 0)
 {
     if (r)
     {
         C(n - 1, r - 1, c * 10 + n);
         if (r < n)
-        {
             C(n - 1, r, c);
-        }
     }
     else
-    {
         std::cout << c << std::endl;
-    }
 }
 
 int main()
